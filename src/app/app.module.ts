0694b729ee95fcc8582ed7ed445df45c208b8997
AbbaciKhaled <47677757+AbbaciKhaled@users.noreply.github.com>
2020-01-30 17:17:17 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,6 @@ import { BoxOfficeComponent } from './components/box-office/box-office.component
 import { MovieComponent } from './components/movie/movie.component';
 import { Exercice1Component } from './components/exercice1/exercice1.component';
 import { HorlogeComponent } from './components/horloge/horloge.component';
-import { afficherTextComponent } from './components/exosSupplementaires/afficher-text/afficher-text.component';
-import { ButtonCompComponent } from './components/exosSupplementaires/button-comp/button-comp.component';
-import { Exercice3Component } from './components/exosSupplementaires/exercice3/exercice3.component';
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,10 +17,7 @@ import { Exercice3Component } from './components/exosSupplementaires/exercice3/e
     BoxOfficeComponent,
     MovieComponent,
     Exercice1Component,
-    HorlogeComponent,
-    afficherTextComponent,
-    ButtonCompComponent,
-    Exercice3Component
+    HorlogeComponent
   ],
   imports: [
     BrowserModule,
