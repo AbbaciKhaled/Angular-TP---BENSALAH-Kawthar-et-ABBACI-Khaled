90e0f7f3421eec55087797f587529f2166f006b1
AbbaciKhaled <47677757+AbbaciKhaled@users.noreply.github.com>
2020-01-30 16:27:34 +0100

    Add files via upload

---
+++ src/app/components/exosSupplementaires/afficher-text/afficher-text.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {DataButton} from '../../model/dataButton';
+import { Button } from '../../model/button';
+
+@Component({
+  selector: 'app-afficher-text',
+  templateUrl: './afficher-text.component.html',
+  styleUrls: ['./afficher-text.component.scss']
+})
+export class AfficherTextComponent implements OnInit {
+
+  @Input() text: Text = null;
+
+  buttons: Button[];
+
+  buttonAffiche : string = 'Aucun Boutton n\'est cliqué';
+
+  constructor() { }
+
+
+  ngOnInit() {
+    this.buttons = DataButton.list;
+
+
+  }
+
+  maj($event){
+    this.buttonAffiche = $event;
+  }
+
+
+}
---
+++ src/app/components/exosSupplementaires/exercice3/exercice3.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-exercice3',
+  templateUrl: './exercice3.component.html',
+  styleUrls: ['./exercice3.component.scss']
+})
+export class Exercice1Component implements OnInit {
+
+  text: Text;
+
+
+ 
+
+  constructor() {
+
+
+  }
+
+  ngOnInit() {
+  
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
